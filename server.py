8ecce6807a43a2fb1f319d91334eebfb14b1e55b
Manuel Cortez <94556559+cortezxm@users.noreply.github.com>
2023-10-08 11:45:41 -0600

    Update server.py
    Signed-off-by: Manuel Cortez <94556559+cortezxm@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,21 +1,171 @@
-import os
-from flask import Flask, send_from_directory, render_template, redirect
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import json
+from bson import json_util
 
 app = Flask(__name__)
 
-port = int(os.environ.get("PORT", 5000))
+uri = "mongodb+srv://node_connection:wxcqjexeYQgshxjF@animals.fbfy7o7.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(uri, server_api=ServerApi('1'))
 
-@app.route('/static/<path:path>')
-def serve_static(path):
-    return send_from_directory('static', path)
+db = client['animals']
 
-@app.route('/')
-def home():
-   return render_template('index.html')
+animals = db['animals']
+water = db['water']
 
-@app.route('/<path:path>')
-def all_routes(path):
-    return redirect('/')
+@app.route('/api/animals/', methods=['GET'])
+def get_animals():
+    lat = float(request.args.get('lat'))
+    lng = float(request.args.get('lng'))
 
-if __name__ == "__main__":
-    app.run(port=port)
+    aggregation = [
+    {
+        '$geoNear': {
+            'near': {
+                'type': 'Point', 
+                'coordinates': [
+                    lng, lat
+                ]
+            }, 
+            'distanceField': 'distance', 
+            'maxDistance': 50000, 
+            'query': {}, 
+            'spherical': False
+        }
+    }, {
+        '$group': {
+            '_id': {
+                'name': '$name'
+            }, 
+            'unique': {
+                '$first': '$$ROOT'
+            }
+        }
+    }, {
+        '$replaceRoot': {
+            'newRoot': '$unique'
+        }
+    }, {
+        '$sort': {
+            'kingdom': 1
+        }
+    }, {
+        '$limit': 100
+    }, {
+        '$project': {
+            'location': {
+                '$ifNull': [
+                    '$location', '$$REMOVE'
+                ]
+            }, 
+            'name': {
+                '$ifNull': [
+                    '$name', '$$REMOVE'
+                ]
+            }, 
+            'kingdom': {
+                '$ifNull': [
+                    '$kingdom', '$$REMOVE'
+                ]
+            }, 
+            'phylum': {
+                '$ifNull': [
+                    '$phylum', '$$REMOVE'
+                ]
+            }, 
+            'class': {
+                '$ifNull': [
+                    '$class', '$$REMOVE'
+                ]
+            }, 
+            'country_full': {
+                '$ifNull': [
+                    '$country_full', '$$REMOVE'
+                ]
+            }, 
+            'state_province': {
+                '$ifNull': [
+                    '$state_province', '$$REMOVE'
+                ]
+            }, 
+            'county': {
+                '$ifNull': [
+                    '$county', '$$REMOVE'
+                ]
+            }, 
+            'locality': {
+                '$ifNull': [
+                    '$locality', '$$REMOVE'
+                ]
+            }, 
+            'how_to_cite_source': {
+                '$ifNull': [
+                    '$how_to_cite_source', '$$REMOVE'
+                ]
+            }, 
+            'consult_year': {
+                '$ifNull': [
+                    '$consult_year', '$$REMOVE'
+                ]
+            }, 
+            'state': {
+                '$cond': [
+                    {
+                        '$eq': [
+                            '$state', None
+                        ]
+                    }, 'Not in danger', '$state'
+                ]
+            }
+        }
+    }
+]
+    
+    result = animals.aggregate(aggregation)
+    print(result)
+
+    json_result = []
+    for document in result:
+        json_result.append(json.loads(json_util.dumps(document)))
+
+    return jsonify(json_result), 200
+
+
+@app.route('/api/water/', methods=['GET'])
+def get_water_bodies():
+    lat = float(request.args.get('lat'))
+    lng = float(request.args.get('lng'))
+
+    print(f"lat: {lat}, lng: {lng}")
+
+    aggregation = [
+    {
+        '$geoNear': {
+            'near': {
+                'type': 'Point', 
+                'coordinates': [lng, lat]
+            }, 
+            'distanceField': 'distance', 
+            'maxDistance': 50000, 
+            'query': {}, 
+            'spherical': False
+        }
+    }, {
+        '$sort': {'distance': 1}
+    }, {
+        '$limit': 1
+        }
+    ]
+
+    result = water.aggregate(aggregation)
+
+    json_result = []
+    for document in result:
+        json_result.append(json.loads(json_util.dumps(document)))
+
+    return jsonify(json_result), 200
+    
+
+if __name__ == '__main__':
+    app.run(debug=False)
